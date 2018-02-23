38d04970d7481499e90764e1ffdda70120359918
root <root@localhost.localdomain>
2018-02-24 06:39:32 +0700

    Added new error messages, now errors numreated. Some bugs fixed.

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <math.h>
 
 //Цветовые коды, можно отключить
 char* TC_W = "\033[0m";
@@ -56,10 +57,10 @@ int searchsym(char* t, int l, char* sym)
     return c;
 }
 
-void error(char* red, char* white)
+void error(char* red, char* white, int errorcode)
 {
     printf("%s(Ошибка) %s%s%s%s\n",BC_W, BC_R, red, TC_W, white);
-    exit(0);
+    exit(errorcode);
 }
 
 void warning(char* yellow, char* white)
@@ -130,7 +131,6 @@ int main(int argc, char *argv[])
     */
     int from[2];
     int to[2];
-    int castle[2];
     /*
 	Ячейки:
 	<from> - стартовая клетка
@@ -163,7 +163,7 @@ int main(int argc, char *argv[])
     void printfield(char* [8][8]);
     int tokenlen(char* );
     int searchsym(char* , int, char*);
-    void error(char*, char*);
+    void error(char*, char*, int);
     void warning(char*, char*);
     
     //КОД
@@ -173,7 +173,7 @@ int main(int argc, char *argv[])
     char *temp;
     char filename[256];
     int loop = 0;
-    int count, i;
+    int count, i, imove, jmove;
     //Производим ввод файла с записью ходов
     printf("%sАдрес исходного файла:%s\n",BG_W,TC_W);
     scanf("%s",filename);
@@ -181,6 +181,8 @@ int main(int argc, char *argv[])
     if (file){
 	printf("\n");
 	//Переключаем строки
+	printfield(field);
+	printf("\n");
 	while(1){
 	    //Заносим строку в переменную
 	    temp = fgets(step,sizeof(step),file);
@@ -199,7 +201,7 @@ int main(int argc, char *argv[])
 		token = strtok_r(step, " ", &last);
 		for(i = 0; i <= count; i++){
 		    //Выводим ход в правильном виде, учитывая знак новой строки в конце
-		    printf ("%s", token);
+		    printf ("%s%d. %s%s\n",BC_Y, loop+1, BC_W, token);
 		    if (i != count){
 			printf("\n");
 		    }
@@ -219,39 +221,59 @@ int main(int argc, char *argv[])
 		    } else if (tolower(token[0]) == token[0]){
 			figure = "P";
 		    } else {
-			error("Невозможно определить фигуру",", фигуры соответствующие формату: K, Q, R, N, B или не указывайте ничего для определения пешки");
+			error("Невозможно определить фигуру",", фигуры соответствующие формату: K, Q, R, N, B или не указывайте ничего для определения пешки", 2);
 		    }
 		    //Фигура нам известа, определяем тип хода, используем для этого подготовленную заранее ф-ию(int searchsym)
-		    if ((searchsym(token, len, "-") == 2) || (searchsym(token, len, "-") == 1)){
-			type = searchsym(token, len, "x");
-		    } else if (searchsym(token, len, "x") == 1){
+		    if ((searchsym(token, len, "-") == 1) && (searchsym(token, len, "0") == 0)){
+			type = 1;
+		    } else if ((searchsym(token, len, "x") == 1) && (searchsym(token, len, "0") == 0)){
 			type = 0;
+		    } else if ((searchsym(token, len, "0") == 2) && (searchsym(token, len, "-") == 1)){
+			type = 2;
+		    } else if ((searchsym(token, len, "0") == 3) && (searchsym(token, len, "-") == 2)){
+			type = 3;
 		    } else {
-			error("Ход нестандартного формата",", попробуйте: e2-e4, e2xe4 или a1-e1-b1");	
+			error("Ход нестандартного формата",", попробуйте: e2-e4, e2xe4, 0-0, 0-0-0",3);
 		    }
-		    //В зависимости от типа хода, узнаём, что и куда переместить
-		    int ps;
-		    //Проверяем является ли фигура пешкой
-		    if (*(figure) == 80){
-			ps = 1;
-		    } else {
-			ps = 0;
-		    }
-		    //Делим ход на точку отправления, прибытия и в случае с рокировкой на точку кастла. Переводим всё это в массивы целочисленного типа
-		    from[0] = token[1-ps]-97;
-		    from[1] = 8-(token[2-ps]-48);
-		    to[0] = token[4-ps]-97;
-		    to[1] = 8-(token[5-ps]-48);
-		    if (type == 2){
-			castle[0] = token[7-ps]-97;
-			castle[1] = 8-(token[8-ps]-48);
-		    }
-		    //Обработка ходов
-		    if (type != 2){
+		    if (type < 2){
+			//В зависимости от типа хода, узнаём, что и куда переместить
+			int ps;
+			//Проверяем является ли фигура пешкой
+			if (*(figure) == 80){
+			    ps = 1;
+			} else {
+			    ps = 0;
+			}
+			//Делим ход на точку отправления и прибытия. Переводим всё это в массивы целочисленного типа
+			from[0] = token[1-ps]-97;
+			from[1] = 8-(token[2-ps]-48);
+			to[0] = token[4-ps]-97;
+			to[1] = 8-(token[5-ps]-48);
+			//Проверка на правильность действия
+			if ((*(field[to[1]][to[0]]) == 32) && (type == 0)){
+			    error("Невозможно произвести захват пустой клетки", ", для обозначения тихого хода используйте формат: e2-e4, вместо e2xe4",5);
+			}
+			//Проверка на правильность фигуры
+			if (toupper(*field[from[1]][from[0]]) != *figure){
+			    error("Фигура в клетке не соответствует взятой", ", используемая фигура должна соответствовать фигуре на столе",6);
+			}
+			//Проверка на выход за пределы поля
+			if ((from[0] > 7) || (from[1] > 7) || (to[0] > 7) || (to[1] > 7) ||  (from[0] < 0) || (from[1] < 0) || (to[0] < 0) || (to[1] < 0)){
+			    error("Ход совершается за пределами поля", ", допустимые значения (a-f,1-8)",4);
+			}
+			//Проверка на соответствие хода фигуре
+			imove = abs(from[1] - to[1]);
+			jmove = abs(from[0] - to[0]);
+			
+			//????
+			
+			//Обработка ходов
 			field[to[1]][to[0]] = field[from[1]][from[0]];
 			field[from[1]][from[0]] = " ";
 		    } else {
-			
+		    
+			//????
+		    
 		    }
 		    //Конец работы с ходами, далее вывод
 		    printfield(field);
@@ -271,7 +293,7 @@ int main(int argc, char *argv[])
 	}
     } else {
 	//Ошибка говорящая сама за себя
-	error("Файл по такому адресу не найден","");
+	error("Файл по такому адресу не найден","",1);
     }
     return 0;
 }
